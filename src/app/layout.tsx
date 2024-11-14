371124343edce6b16157db6f6b1892a758c8889c
Your name here <elia.orsini@hotmail.com>
2024-11-14 13:13:02 +0000

    chore: linting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,10 +11,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-  params,
 }: Readonly<{
   children: React.ReactNode;
-  params: { slug: string };
 }>) {
   return (
     <html lang="en">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Canvas3D from "@/components/3D/Canvas3D";
-
 export default function Home() {
   return <div className=""></div>;
 }
